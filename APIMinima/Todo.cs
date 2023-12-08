862432b1e144cb536cdacc1d5ae6dc95a45ae9b7
Roberto <roberto@tiredirect.com.mx>
2023-12-08 13:05:11 -0600

    Agregar archivos de proyecto.

---
+++ APIMinima/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace APIMinima
+{
+    public class Todo
+    {
+        public int id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public string? Secret { get; set; }
+    }
+}
---
+++ APIMinima/TodoItemDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace APIMinima
+{
+    public class TodoItemDTO
+    {
+
+        public int id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+
+        public TodoItemDTO() { }
+
+        public TodoItemDTO(Todo todoItem) =>
+            (id, Name, IsComplete) = (todoItem.id, todoItem.Name, todoItem.IsComplete);
+
+    }
+}
